96fffe9a753e6e6dbffcc94fc8c2a852049c5930
Tom Tijerina <ttijerina@wirediq.com>
2019-10-01 09:50:37 -0400

    Video 18 lesson

---
+++ Playground/1-json.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const filename = '1-json.json'
+
+const data = JSON.parse(fs.readFileSync(filename).toString())
+data.name = 'Tom T'
+data.age = "37"
+
+const dataJSON = JSON.stringify(data)
+fs.writeFileSync(filename, dataJSON)
