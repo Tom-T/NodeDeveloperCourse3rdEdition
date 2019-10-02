36cb1169a749e3d9112f4d0b0c14dcac3705dec4
Tom Tijerina <ttijerina@wirediq.com>
2019-10-02 11:44:03 -0400

    Moved geocode to its own file.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
-const request = require("request");
 const chalk = require("chalk");
+const geocode = require("./utils/geocode");
 
-const url =
-  "https://api.darksky.net/forecast/8faf1fcb572732ca282d61acc5180cfb/37.8267,-122.4233?units=us";
+// const url =
+//   "https://api.darksky.net/forecast/8faf1fcb572732ca282d61acc5180cfb/37.8267,-122.4233?units=us";
 
 // const locationUrl =
 //   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHRpamVyaW5hIiwiYSI6ImNrMTljODRkbzA5ZzEzZG50OWpqZzRiZWMifQ.2AlOEN_CcB0rKyuGb9NTaw&limit=1";
@@ -57,35 +57,17 @@ const url =
 //   }
 // });
 
-const geocode = (address, callback) => {
-  const url =
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-    encodeURIComponent(address) +
-    ".json?access_token=pk.eyJ1IjoidHRpamVyaW5hIiwiYSI6ImNrMTljODRkbzA5ZzEzZG50OWpqZzRiZWMifQ.2AlOEN_CcB0rKyuGb9NTaw&limit=1";
-
-  request({ url: url, json: true }, (error, response) => {
-    if (error) {
-      callback("Unable to connect to location services!");
-    } else if (response.body.features.length) {
-      callback(undefined, {
-        location: response.body.features[0].place_name,
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0]
-      });
-    } else {
-      callback("Address not found!");
-    }
-  });
-};
-
-
 
 geocode("Tampa", (error, data) => {
   if (error) {
     console.log(chalk.red(error));
   } else {
     console.log(
-      data.location + "\n\t Lat:" + data.latitude + " and Long:" + data.longitude
+      data.location +
+        "\n\t Lat:" +
+        data.latitude +
+        " and Long:" +
+        data.longitude
     );
   }
 });
