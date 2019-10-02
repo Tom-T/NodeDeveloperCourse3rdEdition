ab2f96fca0a14148ded90a057e7911846721f40d
Tom Tijerina <ttijerina@wirediq.com>
2019-10-02 16:47:33 -0400

    Video 49 Challenge.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,26 +2,34 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 
-const publicPath = path.join(__dirname, "../public");
 const app = express();
 
-app.set("view engine", "hbs");
+// Define paths for Express
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve.
 app.use(express.static(publicPath));
 
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather App",
+    title: "Weather",
     name: "Tom"
   });
 });
-
 app.get("/help", (req, res) => {
   res.render("help", {
-    message: "The help message"
+    title: "Help",
+    message: "The help message",
+    name: "Tom"
   });
 });
-
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Page",
