509a6c6ccb9ed7695e1ee41b466242bb340c05b2
Tom Tijerina <ttijerina@wirediq.com>
2019-10-02 10:59:56 -0400

    Tested removing {} from single function

--- Playground/4-callbacks.js
+++ Playground/4-callbacks.js
@@ -19,18 +19,16 @@
 
 
 
-//
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
 
+//Both work.
+
+// const add = (x, y, callback) => {
+//   setTimeout(() => {
+//     callback(x + y);
+//   }, 2000);
+// };
 const add = (x, y, callback) => {
-  setTimeout(() => {
-    callback(x + y);
-  }, 2000);
+  setTimeout(() => callback(x + y), 2000);
 };
 
 add(1, 4, (sum) => {
