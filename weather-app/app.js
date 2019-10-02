1e099ab3f49743b8d623e1824f8c2419e79830fc
Tom Tijerina <ttijerina@wirediq.com>
2019-10-02 10:35:13 -0400

    Added error handling as per video 34.

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,40 +4,58 @@ const chalk = require("chalk");
 const url =
   "https://api.darksky.net/forecast/8faf1fcb572732ca282d61acc5180cfb/37.8267,-122.4233?units=us";
 
+// const locationUrl =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHRpamVyaW5hIiwiYSI6ImNrMTljODRkbzA5ZzEzZG50OWpqZzRiZWMifQ.2AlOEN_CcB0rKyuGb9NTaw&limit=1";
+
 const locationUrl =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHRpamVyaW5hIiwiYSI6ImNrMTljODRkbzA5ZzEzZG50OWpqZzRiZWMifQ.2AlOEN_CcB0rKyuGb9NTaw&limit=1";
-("pk.eyJ1IjoidHRpamVyaW5hIiwiYSI6ImNrMTljODRkbzA5ZzEzZG50OWpqZzRiZWMifQ.2AlOEN_CcB0rKyuGb9NTaw");
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/Tampa.json?access_token=pk.eyJ1IjoidHRpamVyaW5hIiwiYSI6ImNrMTljODRkbzA5ZzEzZG50OWpqZzRiZWMifQ.2AlOEN_CcB0rKyuGb9NTaw&limit=1";
+
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log(chalk.red("Unable to connect to weather service!"));
+//   } else if (response.body.error) {
+//     console.log(chalk.red("Unable to find location"));
+//   } else {
+//     //console.log(response)
+//     temp = response.body.currently.temperature;
+//     rain = response.body.currently.precipProbability;
+//     returnString = "It is currently ";
+//     if (temp >= 90) {
+//       returnString += chalk.red(temp);
+//     } else if (temp >= 70) {
+//       returnString += chalk.green(temp);
+//     } else {
+//       returnString += chalk.blue(temp);
+//     }
+
+//     returnString += " degrees out. There is a ";
+
+//     if (rain >= 60) {
+//       returnString += chalk.red(rain);
+//     } else if (rain >= 20) {
+//       returnString += chalk.yellow(rain);
+//     } else {
+//       returnString += chalk.green(rain);
+//     }
+
+//     returnString += " chance of rain.";
+//     console.log(returnString);
+//   }
+// });
+
 request({ url: locationUrl, json: true }, (error, response) => {
-  // request({ url: url, json: true }, (error, response) => {
-  // //console.log(response)
-  // temp = response.body.currently.temperature;
-  // rain = response.body.currently.precipProbability;
-  // returnString = "It is currently ";
-  // if (temp >= 90) {
-  //   returnString += chalk.red(temp);
-  // } else if (temp >= 70) {
-  //   returnString += chalk.green(temp);
-  // } else {
-  //   returnString += chalk.blue(temp);
-  // }
-
-  // returnString += " degrees out. There is a ";
-
-  // if (rain >= 60) {
-  //   returnString += chalk.red(rain);
-  // } else if (rain >= 20) {
-  //   returnString += chalk.yellow(rain);
-  // } else {
-  //   returnString += chalk.green(rain);
-  // }
-
-  // returnString += " chance of rain.";
-  // console.log(returnString);
-
-  console.log(
-    "Lat: " +
-      response.body.features[0].center[1] +
-      "\nLon: " +
-      response.body.features[0].center[0]
-  );
+  if (error) {
+    console.log(chalk.red("Unable to connect to weather service!"));
+  } else if (response.body.features.length) {
+    console.log(
+      "City: "+
+      response.body.features[0].place_name + 
+      " \n\tLat: " +
+        response.body.features[0].center[1] +
+        "\n\tLon: " +
+        response.body.features[0].center[0]
+    );
+  } else {
+    console.log(chalk.red("Address not found!"));
+  }
 });
