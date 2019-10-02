9edbe05e47c42944d6f2468c47552a0d92953f33
Tom Tijerina <ttijerina@wirediq.com>
2019-10-02 09:29:55 -0400

    Challenge 32. Added some statements to add color to the mix.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,32 @@
 const request = require("request");
+const chalk = require("chalk");
 
 const url =
   "https://api.darksky.net/forecast/8faf1fcb572732ca282d61acc5180cfb/37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
+request({ url: url, json: true }, (error, response) => {
   //console.log(response)
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+  temp = response.body.currently.temperature;
+  rain = response.body.currently.precipProbability;
+  returnString = "It is currently ";
+  if (temp >= 90) {
+    returnString += chalk.red(temp);
+  } else if (temp >= 70) {
+    returnString += chalk.green(temp);
+  } else {
+    returnString += chalk.blue(temp);
+  }
+
+  returnString += " degrees out. There is a ";
+
+  if (rain >= 60) {
+    returnString += chalk.red(rain);
+  } else if (rain >= 20) {
+    returnString += chalk.yellow(rain);
+  } else {
+    returnString += chalk.green(rain);
+  }
+
+  returnString += " chance of rain.";
+  console.log(returnString);
 });
