74379922163a3a2d7a8e666c0f1b72170d7bb929
Tom Tijerina <ttijerina@wirediq.com>
2019-10-03 13:42:11 -0400

    Saving as section was closed.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,18 +5,18 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-
-
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
   fetch("/weather?address=" + search.value).then(response => {
     response.json().then(data => {
       if (data.error) {
-        messageOne.textContent = ""
-        return messageTwo.textContent = data.error;
+        messageOne.textContent = "";
+        return (messageTwo.textContent = data.error);
       }
-      messageTwo.textContent = "Location: " + data.location + "\nForecast " + data.forecast;
+      messageTwo.textContent =
+        "Location: " + data.location + "\nForecast " + data.forecast;
       messageOne.textContent = "";
     });
   });
