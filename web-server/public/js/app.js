ac8e0883d60fb96cc7294abd5bab8532a5e7c0ef
Tom Tijerina <ttijerina@wirediq.com>
2019-10-03 11:41:20 -0400

    Video 58 challenge.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,15 @@
 console.log("Client side javascript loaded.");
 
-fetch("/weather?address=boston").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      return console.log(data.error);
-    }
-    console.log("Location: " + data.location + "\nForecast" + data.forecast);
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch("/weather?address=" + search.value).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        return console.log(data.error);
+      }
+      console.log("Location: " + data.location + "\nForecast" + data.forecast);
+    });
   });
 });
