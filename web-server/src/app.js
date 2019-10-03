a87df26d4b2bcf14dc5353b9f2059cfd034dfea5
Tom Tijerina <ttijerina@wirediq.com>
2019-10-03 10:01:46 -0400

    Video 54 Challenge.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -12,7 +12,7 @@ const partialPath = path.join(__dirname, "../templates/partials");
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-hbs.registerPartials(partialPath)
+hbs.registerPartials(partialPath);
 
 // Setup static directory to serve.
 app.use(express.static(publicPath));
@@ -31,10 +31,10 @@ app.get("/help", (req, res) => {
   });
 });
 app.get("/help/*", (req, res) => {
-    res.render("404", {
-      message: "Help article not found",
-      name: "Tom"
-    });
+  res.render("404", {
+    message: "Help article not found",
+    name: "Tom"
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about", {
@@ -43,9 +43,25 @@ app.get("/about", (req, res) => {
   });
 });
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "Address must be provided." });
+  }
+  const address = req.query.address
   res.send({
     forecast: "40",
-    location: "Tampa"
+    location: "Tampa",
+    address
+  });
+});
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({ error: "No search string provided!" });
+  }
+
+  const value = req.query;
+  res.send({
+    products: [],
+    value
   });
 });
 app.get("*", (req, res) => {
