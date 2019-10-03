0147548b6fc2f9f42f09b65dfcb1489da62f1064
Tom Tijerina <ttijerina@wirediq.com>
2019-10-03 11:30:33 -0400

    Video 57 challenge

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
-console.log("Client side javascript loaded.")
+console.log("Client side javascript loaded.");
+
+fetch("/weather?address=boston").then(response => {
+  response.json().then(data => {
+    if (data.error) {
+      return console.log(data.error);
+    }
+    console.log("Location: " + data.location + "\nForecast" + data.forecast);
+  });
+});
