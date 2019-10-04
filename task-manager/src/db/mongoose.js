ce701acfaa51a10e5a2065134659a7db9cc85c9d
Tom Tijerina <ttijerina@wirediq.com>
2019-10-04 14:10:57 -0400

    Video 86 challenge

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,43 +8,88 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("user", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password: {
+    required: true,
+    trim: true,
+    type: String,
+    validate(value) {
+      if (value.length <= 6) {
+        throw new Error("Password must contain more then 6 characters");
+      } else if (value.includes("password")) {
+        throw new Error('Password can\'t contain the word "password"');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
   }
 });
-
-const Task = mongoose.model("task", {
-  description: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  }
+const user = new User({
+  name: "Tom2",
+  password: "securepass",
+  email: "No@Email.co"
 });
 
-const task1 = new Task({
-  description: "Learn to program",
-  completed: false
-})
-task1
+user
   .save()
-  .finally(() => {
-    console.log("When does this run?");
-  })
   .then(() => {
-    console.log("Saved!");
+    console.log("Record Saved");
   })
   .catch(e => {
-    console.log("Error: ", e);
-  })
-  .finally(() => {
-    console.log("When does this run?2");
-  })
-  .then(() => {
-    console.log("Saved!");
+    console.log("Error", e);
   });
+
+//   const Task = mongoose.model("task", {
+//   description: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   }
+// });
+
+// const task1 = new Task({
+//   description: "Learn to program",
+//   completed: false
+// });
+// task1
+//   .save()
+//   .finally(() => {
+//     console.log("When does this run?");
+//   })
+//   .then(() => {
+//     console.log("Saved!");
+//   })
+//   .catch(e => {
+//     console.log("Error: ", e);
+//   })
+//   .finally(() => {
+//     console.log("When does this run?2");
+//   })
+//   .then(() => {
+//     console.log("Saved!");
+//   });
 
 // const me = new User({
 //   name: "Mike",
