dac54fd1109c98d6bb580a2960f9460b8b5c7499
Tom Tijerina <ttijerina@wirediq.com>
2019-10-04 16:19:45 -0400

    Challenge for video 92.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -13,10 +13,33 @@ app.post("/tasks", (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch(e => {
       res.status(400).send(e);
+    });
+});
+app.get("/tasks",
+  (req, res) => {
+    Task.find({})
+      .then(tasks => {
+        res.status(200).send(tasks);
+      })
+      .catch(e => {
+        res.status(404).send();
+      });
+  });
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id
+  Task.findById(_id)
+    .then(task => {
+      if (!task){
+        res.status(404).send();
+      }
+      res.status(200).send(task);
+    })
+    .catch(e => {
+      res.status(500).send();
     });
 });
 
@@ -25,13 +48,34 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch(e => {
       res.status(400).send(e);
     });
 });
 
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then(users => res.status(200).send(users))
+    .catch(e => {
+      res.status(500).send();
+    });
+});
 app.listen(port, () => {
   console.log("Server is up on port ", port);
 });
