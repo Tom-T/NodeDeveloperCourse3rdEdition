c3af8c1efd371017cd88ab73636e6f77f9327e56
Tom Tijerina <ttijerina@wirediq.com>
2019-10-04 10:23:02 -0400

    Video 78 Challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,15 +2,10 @@
 // const MongoClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectID
 
-const {MongoClient, ObjectID} = require("mongodb");
-
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
-
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
@@ -18,68 +13,26 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   }
   const db = client.db(databaseName);
 
-  // db.collection("users").insertOne(
-  //   {
-  //     name: "Tom",
-  //     age: 37
-  //   },
-  //   (error, result) => {
-  //     if (error) {
-  //       return console.log("Unable to insert user");
-  //     }
-
-  //     console.log(result.ops);
+  // db.collection("users").findOne({ _id: new ObjectID("5d973995ddd12c2bc79c2dc9") }, (error, user) => {
+  //   if (error) {
+  //     return console.log("unable to fetch");
   //   }
-  // );
 
-  // db.collection("users").insertMany(
-  //   [
-  //     {
-  //       name: "jan",
-  //       age: 28
-  //     },
-  //     {
-  //       name: "Gunther",
-  //       age: 23
-  //     }
-  //   ],
-  //   (error, result) => {
-  //     if (error) {
-  //       return console.log("Unable to insert documents");
-  //     }
+  //   console.log(user);
+  // });
 
-  //     console.log(result.ops);
-  //   }
-  // );
-
-  // db.collection("tasks").insertMany(
-  //   [
-  //     {
-  //       description: "This is the first completed task.",
-  //       completed: true
-  //     },
-  //     {
-  //       description: "This is the second task.",
-  //       completed: false
-  //     },
-  //     {
-  //       description: "This another task.",
-  //       completed: false
-  //     },
-  //     {
-  //       description: "This finished task.",
-  //       completed: true
-  //     }
-  //   ],
-  //   (error, ops) => {
-  //     if (error) {
-  //       return console.log("Error whlie accessing the DB:\n", error);
-  //     }
-  //     console.log("Database updated without issues!\n", ops);
-  //   }
-  // );
-
-
-
-
+  db.collection("tasks").findOne({}, { sort: [["_id", "desc"]], limit: 1 }, (error, task) => {
+    if (error) {
+      return console.log("unable to fetch");
+    }
+    console.log(task);
+  });
+  console.log("Unfinished tasks:");
+  db.collection("tasks")
+    .find({ completed: false })
+    .toArray((error, tasks) => {
+      tasks.forEach(task => {
+        console.log(task);
+      });
+    });
 });
