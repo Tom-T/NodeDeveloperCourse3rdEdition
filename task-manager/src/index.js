0443ccd6eb2fc363cd5937de9f2adf3d26fbeb70
Tom Tijerina <ttijerina@wirediq.com>
2019-10-07 13:26:33 -0400

    Video101 Challenge

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,129 +1,16 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", async (req, res) => {
-  try {
-    const user = await new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "passowrd", "age"];
-  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+app.use(userRouter);
+app.use(taskRouter);
 
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates!" });
-  }
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-app.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(202).send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-app.post("/tasks", async (req, res) => {
-  try {
-    const task = await new Task(req.body);
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).send(tasks);
-  } catch (e) {
-    res.status(404).send();
-  }
-});
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.status(200).send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-app.patch("/tasks/:id", async (req, res) => {
-  const allowedProperties = ["description", "completed"];
-  const changedProperties = Object.keys(req.body);
-  const isValidProperties = changedProperties.every(property => allowedProperties.includes(property));
-
-  if (!isValidProperties) {
-    return res.status(500).send({ error: "Invalid Propertie(s)" });
-  }
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.status(200).send(task);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-app.delete("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.status(202).send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
 app.listen(port, () => {
   console.log("Server is up on port ", port);
 });
---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const Task = require("../models/task");
+const router = new express.Router();
+
+router.post("/tasks", async (req, res) => {
+  try {
+    const task = await new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+router.patch("/tasks/:id", async (req, res) => {
+  const allowedProperties = ["description", "completed"];
+  const changedProperties = Object.keys(req.body);
+  const isValidProperties = changedProperties.every(property => allowedProperties.includes(property));
+
+  if (!isValidProperties) {
+    return res.status(500).send({ error: "Invalid Propertie(s)" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(202).send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
