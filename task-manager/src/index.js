19704f9f7003b0a8f02da126555f5e89019e2b38
Tom Tijerina <ttijerina@wirediq.com>
2019-10-07 11:12:16 -0400

    Video 99 challenge.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,53 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.post("/users", async (req, res) => {
+  try {
+    const user = await new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+app.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "passowrd", "age"];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 app.post("/tasks", async (req, res) => {
   try {
     const task = await new Task(req.body);
@@ -37,34 +84,22 @@ app.get("/tasks/:id", async (req, res) => {
     res.status(500).send();
   }
 });
+app.patch("/tasks/:id", async (req, res) => {
+  const allowedProperties = ["completed"];
+  const changedProperties = Object.keys(req.body);
+  const isValidProperties = changedProperties.every(property => allowedProperties.includes(property));
 
-app.post("/users", async (req, res) => {
-  try {
-    const user = await new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
+  if (!isValidProperties) {
+    return res.status(500).send({ error: "Invalid Propertie(s)" });
   }
-});
-app.get("/users", async (req, res) => {
   try {
-    const users = await User.find({});
-    res.status(200).send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!task) {
       return res.status(404).send();
     }
-    res.status(200).send(user);
+    res.status(200).send(task);
   } catch (e) {
-    res.status(500).send();
+    res.status(500).send(e);
   }
 });
 
