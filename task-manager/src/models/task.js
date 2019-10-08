7c7dfdafca993d904a3459a129f7dd7675917c15
Tom Tijerina <ttijerina@wirediq.com>
2019-10-08 13:58:26 -0400

    Differneces from my code and the tutorial

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const taskSchema = mongoose.Schema(
+const taskSchema = new mongoose.Schema(
   {
     description: {
       type: String,
@@ -21,6 +21,6 @@ const taskSchema = mongoose.Schema(
     timestamps: true
   }
 );
-const Task = mongoose.model("task", taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
