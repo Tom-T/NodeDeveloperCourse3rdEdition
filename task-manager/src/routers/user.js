b0a8013b9d241de85368385c3f3b3e2d1a7ce304
Tom Tijerina <ttijerina@wirediq.com>
2019-10-08 10:56:26 -0400

    Removed an unnecessary line.

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -60,7 +60,6 @@ router.patch("/users/me", auth, async (req, res) => {
     return res.status(400).send({ error: "Invalid updates!" });
   }
   try {
-    const user = await User.findById(req.user._id);
     updates.forEach(update => (req.user[update] = req.body[update]));
     await req.user.save();
     res.status(200).send(req.user);
