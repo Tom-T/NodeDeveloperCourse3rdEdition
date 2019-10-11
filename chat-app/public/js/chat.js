c47626901ff7da7c0283d4c665e51dd849d281ca
Tom Tijerina <ttijerina@wirediq.com>
2019-10-11 11:28:21 -0400

    Got distracted, saving data now.

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,28 +1,36 @@
+const $locationButton = document.querySelector("#send-location");
+const $chatSendButton = document.getElementById("chatSend");
+const $chatText = document.getElementById("chatSendText");
+const $chatWindow = document.getElementById("chatText");
+
 const socket = io();
 socket.on("message", message => {
   console.log(message);
   const p = document.createElement("p");
   p.innerText = message;
-  document.getElementById("chatText").appendChild(p);
+  $chatWindow.appendChild(p);
 });
 
-document.getElementById("chatSend").addEventListener("click", function() {
-  socket.emit("sendMessage", document.getElementById("chatSendText").value, error => {
+$chatSendButton.addEventListener("click", function() {
+  $chatSendButton.setAttribute("disabled", "disabled");
+  socket.emit("sendMessage", $chatText.value, error => {
+    $chatSendButton.removeAttribute("disabled");
+    $chatText.value = "";
     if (error) {
       return alert(error);
     }
-    document.getElementById("chatSendText").value = "";
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", e => {
+$locationButton.addEventListener("click", e => {
+  $locationButton.setAttribute("disabled", "disabled");
   if (!navigator.geolocation) {
     return alert("Geolocation not supported by your browser");
   }
   navigator.geolocation.getCurrentPosition(position => {
-    socket.emit("sendLocation", { latitude: position.coords.latitude, longitude: position.coords.longitude }, () =>{
-      alert("Location shared!")
-
+    socket.emit("sendLocation", { latitude: position.coords.latitude, longitude: position.coords.longitude }, () => {
+      alert("Location shared!");
+      $locationButton.removeAttribute("disabled");
     });
   });
 });
