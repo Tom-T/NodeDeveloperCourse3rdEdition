e17f07a972379a44dea02abce4d698252e80699e
Tom Tijerina <ttijerina@wirediq.com>
2019-10-11 08:21:17 -0400

    video challenge.

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,12 @@
+const socket = io()
+socket.on("message", (message) =>{
+  console.log(message)
+} )
+// socket.on("countUpdated", (count) => {
+// console.log("The count has been updated! ", count)
+// })
+
+// document.querySelector("#increment").addEventListener("click", () => {
+// console.log("Clicked")
+// socket.emit("increment")
+// } )
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,10 +1,28 @@
-const express = require("express");
 const path = require("path");
+const http = require("http")
+const express = require("express");
+const socketio = require("socket.io")
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port);
+let count = 0
+
+io.on("connection", (socket) => {
+  console.log("new websocket connection")
+  socket.emit("message", "Welcome!")
+  // socket.emit("countUpdated", count)
+  // socket.on("increment", () => {
+  //   count++
+  //   //socket.emit("countUpdated", count) //sincle connection
+  //   io.emit("countUpdated", count); //every connection
+  // })
+})
+
+server.listen(port);
